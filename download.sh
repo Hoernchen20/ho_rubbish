fcb4a63c96ca1d34d111117878ae9b6c6baa183d
Hoernchen20 <felix@horn-witten.de>
2020-07-26 16:12:32 +0200

    fix shebang

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-!/bin/bash
+#!/bin/bash
 arm-none-eabi-objcopy -O ihex bin/bluepill/ho_rubbish.elf bin/bluepill/ho_rubbish.hex
 ~/Downloads/stm32flash/./stm32flash -w bin/bluepill/ho_rubbish.hex -v /dev/ttyUSB0 -b 576000
